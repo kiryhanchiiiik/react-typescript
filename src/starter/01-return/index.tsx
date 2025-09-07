ac85ff2fc1941c5dd366b108ad532e4078c4c588
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-07 12:38:43 +0300

    Component Return

--- src/starter/01-return/index.tsx
+++ src/starter/01-return/index.tsx
@@ -1,9 +1,11 @@
-function Component() {
+function Component(): JSX.Element | null | string {
   return (
     <div>
       <h2>React & Typescript</h2>
       <h2>Return Type</h2>
     </div>
   );
+  return null;
+  return "hello";
 }
 export default Component;
