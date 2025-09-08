296b87a0e855c543a9bdc26e9ca3e1274ba855b1
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-08 15:40:42 +0300

    State - Custom List

--- src/starter/03-state/index.tsx
+++ src/starter/03-state/index.tsx
@@ -1,9 +1,34 @@
 import { useState } from "react";
 
+type Link = {
+  id: number;
+  url: string;
+  text: string;
+};
+
+const navLinks = [
+  {
+    id: 1,
+    url: "some url",
+    text: "some text",
+  },
+  {
+    id: 2,
+    url: "some url",
+    text: "some text",
+  },
+  {
+    id: 3,
+    url: "some url",
+    text: "some text",
+  },
+];
+
 function Component() {
   const [text, setText] = useState("shakeAndBake");
   const [number, setNumber] = useState(1);
   const [list, setList] = useState<string[]>([]);
+  const [links, setLinks] = useState<Link[]>(navLinks);
   return (
     <div>
       <h2 className="mb-1">React & Typescript</h2>
@@ -14,6 +39,7 @@ function Component() {
           // setNumber('23');
           setList(["hello", "damn"]);
           // setList([1, 2]);
+          setLinks([...links, { id: 4, url: "hello", text: "hello" }]);
         }}
       >
         click me
