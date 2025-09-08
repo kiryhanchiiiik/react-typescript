7dcb5ac39c17afc6007110c7b2dcdab453108100
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-08 15:35:52 +0300

    State

--- src/starter/03-state/index.tsx
+++ src/starter/03-state/index.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 function Component() {
+  const [text, setText] = useState("shakeAndBake");
+  const [number, setNumber] = useState(1);
+  const [list, setList] = useState<string[]>([]);
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>State</h2>
+      <h2 className="mb-1">React & Typescript</h2>
+      <button
+        className="btn btn-center"
+        onClick={() => {
+          // setText(1);
+          // setNumber('23');
+          setList(["hello", "damn"]);
+          // setList([1, 2]);
+        }}
+      >
+        click me
+      </button>
     </div>
   );
 }
