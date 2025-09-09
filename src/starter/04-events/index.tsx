2e94e36ab7407fc805927a798e5b347c9aa89fc9
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-09 09:28:28 +0300

    Events - Change Event

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
+
 function Component() {
+  const [text, setText] = useState("");
+  const [email, setEmail] = useState("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    setEmail(e.target.value);
+  };
   return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Events</h2>
-    </div>
+    <section>
+      <h2>events example</h2>
+      <form className="form">
+        <input
+          type="text"
+          className="form-input mb-1"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          className="form-input mb-1"
+          value={email}
+          onChange={handleChange}
+        />
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </section>
   );
 }
 export default Component;
