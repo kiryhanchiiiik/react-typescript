cf0fdbd246b2374c8ad2004f2b2ac393252a6330
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-09 11:40:15 +0300

    Events - Form Event

--- src/starter/04-events/index.tsx
+++ src/starter/04-events/index.tsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
 
+type Person = {
+  name: string;
+};
+
 function Component() {
   const [text, setText] = useState("");
   const [email, setEmail] = useState("");
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
     setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    // const formData = new FormData(e.target as HTMLFormElement);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    const text = formData.get("text") as string;
+    const person: Person = { name: data.text as string };
   };
   return (
     <section>
       <h2>events example</h2>
-      <form className="form">
+      <form onSubmit={handleSubmit} className="form">
         <input
           type="text"
+          name="text"
           className="form-input mb-1"
           value={text}
           onChange={(e) => {
@@ -20,7 +35,8 @@ function Component() {
           }}
         />
         <input
-          type="text"
+          type="email"
+          name="email"
           className="form-input mb-1"
           value={email}
           onChange={handleChange}
