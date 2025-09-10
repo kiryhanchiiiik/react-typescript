cc3a4b0befe612b829d1b21c83d1971567076566
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-10 13:35:39 +0300

    Challenge - First Solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Component from "./final/05-challenge";
-
+// import Component from "./final/05-challenge";
+import Component from "./starter/05-challenge";
 function App() {
   return (
     <main>
+      {/* <Component type="basic" name="susan" /> */}
+      {/* <Component type="advanced" name="anna" email="anna@gmail.com" /> */}
+      <Component type="advanced" name="susan" email="susan@gmail.com" />
       <Component type="basic" name="susan" />
-      <Component type="advanced" name="anna" email="anna@gmail.com" />
     </main>
   );
 }
--- src/starter/05-challenge/index.tsx
+++ src/starter/05-challenge/index.tsx
@@ -1,9 +1,21 @@
-function Component() {
+type ProfileCardProps = {
+  type: "basic" | "advanced";
+  name: string;
+  email?: string;
+};
+
+function Component(props: ProfileCardProps) {
+  const { type, name, email } = props;
+
+  const alertType = type === "basic" ? "success" : "danger";
+
+  const className = `alert alert-${alertType}`;
+
   return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Challenge</h2>
-    </div>
+    <article className={className}>
+      <h2>user: {name}</h2>
+      {email && <h2>email: {email}</h2>}
+    </article>
   );
 }
 export default Component;
