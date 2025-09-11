72b375e3252be46a12a384536580977ae6664697
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-11 17:51:11 +0300

    useReducer - Counter

--- src/starter/07-reducers/index.tsx
+++ src/starter/07-reducers/index.tsx
@@ -5,17 +5,17 @@ function Component() {
   const [state, dispatch] = useReducer(counterReducer, initialState);
   return (
     <div>
-      <h2>Count: 0</h2>
-      <h2>Status: Active</h2>
+      <h2>Count: {state.count}</h2>
+      <h2>Status: {state.status}</h2>
 
       <div className="btn-container">
-        <button onClick={() => console.log("increment")} className="btn">
+        <button onClick={() => dispatch({ type: "increment" })} className="btn">
           Increment
         </button>
-        <button onClick={() => console.log("decrement")} className="btn">
+        <button onClick={() => dispatch({ type: "decrement" })} className="btn">
           Decrement
         </button>
-        <button onClick={() => console.log("reset")} className="btn">
+        <button onClick={() => dispatch({ type: "reset" })} className="btn">
           Reset
         </button>
       </div>
--- src/starter/07-reducers/reducer.ts
+++ src/starter/07-reducers/reducer.ts
@@ -8,9 +8,25 @@ export const initialState = {
   status: "pending",
 };
 
+type UpdateCountAction = {
+  type: "increment" | "decrement" | "reset";
+};
+
+type CounterAction = UpdateCountAction;
+
 export const counterReducer = (
   state: CounterState,
-  action: any
+  action: CounterAction
 ): CounterState => {
-  return state;
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+    case "reset":
+      return { ...state, count: state.count * 0 };
+
+    default:
+      return state;
+  }
 };
