6a5ed2ed4207e02a077bb45363336394c45a4d22
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-16 16:42:17 +0300

    Fetch Data - UseEffect and Fetch API

--- src/starter/08-fetch-data/index.tsx
+++ src/starter/08-fetch-data/index.tsx
@@ -1,7 +1,48 @@
+import { useState, useEffect } from "react";
+const url = "https://www.course-api.com/react-tours-project";
+
 function Component() {
+  // tours
+  const [tours, setTours] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(`Failed to fetch tours...`);
+        }
+        const rawData = await response.json();
+        setTours(rawData);
+        console.log(rawData);
+      } catch (error) {
+        const message =
+          error instanceof Error ? error.message : "there was an error...";
+        setIsError(message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (isError) {
+    return <h3>Error {isError}</h3>;
+  }
+
   return (
     <div>
       <h2>React & Typescript</h2>
+      {tours.map((tour) => {
+        return <h2>{tour.name}</h2>;
+      })}
       <h2>Fetch Data</h2>
     </div>
   );
