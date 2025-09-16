ce094636ce7af540b86fe03239fda230a3b227db
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-16 17:01:54 +0300

    Fetch Data - React Query and Axios

--- src/starter/08-fetch-data/types.ts
+++ src/starter/08-fetch-data/types.ts
@@ -1,4 +1,7 @@
 import { z } from "zod";
+import axios from "axios";
+
+const url = "https://www.course-api.com/react-tours-project";
 
 export const tourSchema = z.object({
   id: z.string(),
@@ -10,3 +13,16 @@ export const tourSchema = z.object({
 });
 
 export type Tour = z.infer<typeof tourSchema>;
+
+export const fetchTours = async (): Promise<Tour[]> => {
+  const response = await axios.get(url);
+
+  const result = tourSchema.array().safeParse(response.data);
+
+  if (!result.success) {
+    console.error(result.error);
+    throw new Error("Failed to parse tours");
+  }
+
+  return result.data;
+};
