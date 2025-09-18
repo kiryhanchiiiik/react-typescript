02d4510dc49106a38e8db6bb03f24189698ebd12
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-09-18 09:50:24 +0300

    Tasks Project - Form Setup

--- src/starter/10-tasks/Form.tsx
+++ src/starter/10-tasks/Form.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 const Form = () => {
-  return <div>Form</div>;
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!text) {
+      alert("Please enter a task");
+      return;
+    }
+    // add task
+    console.log(text);
+
+    setText("");
+  };
+
+  return (
+    <form className="form task-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="form-input"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+
+      <button type="submit" className="btn">
+        Add Task
+      </button>
+    </form>
+  );
 };
 
 export default Form;
